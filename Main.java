196849292cd0f8c5a48d8489599ef3640e3f14a8
Frendzyfy <wradegol@mail.ru>
2023-02-14 21:38:50 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,3 @@
-//import java.util.Scanner;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println(sc.nextLong());
-//    }
-//}
-//import java.util.Scanner;
-//class MyProgram{
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        int v = sc.nextInt();
-//        int t = sc.nextInt();
-//        System.out.println(500%1000);
-//    }
-//}
-//class Main{
-//    public static void main(String[] args){
-//        int first = 15;
-//        int second = --first;
-//        second *= 2;
-//        int third = second - 13;
-//        if (first == third){
-//            System.out.println(--second);
-//        }else{
-//            System.out.println(third - first + second);
-//        }
-//    }
-//}
 import java.util.Scanner;
 
 class Calculator{
@@ -187,16 +158,3 @@ class Calculator{
             }
         }
     }
-//class MyProgram{
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        int a = sc.nextInt();
-//        String b = sc.next();
-//        int c = sc.nextInt();
-//        if (a==c) {
-//            System.out.print(a);
-//        }else{
-//            System.out.print(c+a);
-//        }
-//}
-//}
